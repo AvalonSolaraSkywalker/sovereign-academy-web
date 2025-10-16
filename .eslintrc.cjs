b4dcaa1654d42c1e6c826a18e7325b063bc28635
Luke Gatrell <444Luke86@pm.me>
2025-10-16 15:46:09 -0600

    chore: remove duplicate config files; keep authoritative copies at repo root

--- .eslintrc.cjs
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  extends: ['next/core-web-vitals'],
-  // Add any custom rules here, e.g.:
-  // rules: { 'no-console': 'warn' },
-};
--- postcss.config.cjs
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  plugins: {
-    '@tailwindcss/postcss': {},   // Tailwind wrapper
-    autoprefixer: {},             // keep if you use autoprefixer
-  },
-};
